039380464d812ea7ddb5d2627eeccf79fc817233
99winnmin <ojysep9987@gmail.com>
2024-05-03 01:08:48 +0900

    :tada: chore : kopring 게시판 init setting

---
+++ ko-board/src/main/kotlin/com/korine/koboard/KoBoardApplication.kt
@@ -0,0 +1,11 @@
+package com.korine.koboard
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KoBoardApplication
+
+fun main(args: Array<String>) {
+    runApplication<KoBoardApplication>(*args)
+}
---
+++ ko-board/src/test/kotlin/com/korine/koboard/KoBoardApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.korine.koboard
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KoBoardApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
