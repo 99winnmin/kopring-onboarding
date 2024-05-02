a2f8fd270753e2c53b3b2e945cae980c61ce5f1e
99winnmin <ojysep9987@gmail.com>
2024-05-03 01:09:18 +0900

    :memo: feat : swagger check 용 샘플 controller

---
+++ ko-board/src/main/kotlin/com/korine/koboard/SampleController.kt
@@ -0,0 +1,23 @@
+package com.korine.koboard
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class SampleController {
+
+    @GetMapping("/sample")
+    fun sample(): String {
+        return "sample"
+    }
+
+    @PostMapping("/sample")
+    fun postSample(
+            @RequestParam("name") name: String
+    ): String {
+        return "post sample : $name"
+    }
+
+}
