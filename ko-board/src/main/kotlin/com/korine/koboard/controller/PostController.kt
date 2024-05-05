c62bc27253bd00f777e31db589c3406bab911ab0
99winnmin <ojysep9987@gmail.com>
2024-05-05 14:16:54 +0900

    :sparkles: feat : 게시판 API 스펙 설계

---
+++ ko-board/src/main/kotlin/com/korine/koboard/controller/PostController.kt
@@ -0,0 +1,55 @@
+package com.korine.koboard.controller
+
+import com.korine.koboard.dto.*
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.web.bind.annotation.*
+import java.time.LocalDateTime
+
+@RestController
+class PostController {
+
+    @GetMapping("/posts/{id}")
+    fun getPost(
+        @PathVariable id: Long
+    ): PostDetailResponse {
+        return PostDetailResponse(
+            id = 1L,
+            title = "title",
+            content = "content",
+            createdBy = "createdBy",
+            createdAt = LocalDateTime.now()
+        )
+    }
+
+    @GetMapping("/posts")
+    fun getPosts(
+        pageable: Pageable,
+        postSearchRequest: PostSearchRequest
+    ): Page<PostSummaryResponse> {
+        return Page.empty()
+    }
+
+    @PostMapping("/posts")
+    fun createPost(
+        @RequestBody postCreateRequest: PostCreateRequest
+    ): Long {
+        return 1L
+    }
+
+    @PutMapping("/posts/{id}")
+    fun updatePost(
+        @PathVariable id: Long,
+        @RequestBody postUpdateRequest: PostUpdateRequest
+    ): Long {
+        return 1L
+    }
+
+    @DeleteMapping("/posts/{id}")
+    fun deletePost(
+        @PathVariable id: Long,
+        @RequestParam createdBy: String
+    ): Long {
+        return 1L
+    }
+}
