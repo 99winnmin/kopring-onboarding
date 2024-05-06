8d091e83cce10bedae6a476572660ab35912d6f3
99winnmin <ojysep9987@gmail.com>
2024-05-06 16:03:12 +0900

    :sparkles: feat : 게시글 조회 querydsl 쿼리 추가

--- ko-board/src/main/kotlin/com/korine/koboard/repository/PostRepository.kt
+++ ko-board/src/main/kotlin/com/korine/koboard/repository/PostRepository.kt
@@ -1,7 +1,33 @@
 package com.korine.koboard.repository
 
 import com.korine.koboard.domain.Post
+import com.korine.koboard.domain.QPost.post
+import com.korine.koboard.service.dto.PostSearchRequestDto
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageImpl
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport
 
-interface PostRepository : JpaRepository<Post, Long> {
+interface PostRepository : JpaRepository<Post, Long>, CustomPostRepository{
+}
+
+interface CustomPostRepository{
+    fun findPageBy(pageable: Pageable, postSearchRequestDto: PostSearchRequestDto): Page<Post>
+}
+
+class CustomPostRepositoryImpl : CustomPostRepository, QuerydslRepositorySupport(Post::class.java){
+    override fun findPageBy(pageable: Pageable, postSearchRequestDto: PostSearchRequestDto): Page<Post> {
+        val result = from(post)
+            .where(
+                postSearchRequestDto.title?.let { post.title.contains(it) },
+                postSearchRequestDto.createdBy?.let { post.createdBy.eq(it) }
+            )
+            .orderBy(post.createdAt.desc())
+            .offset(pageable.offset)
+            .limit(pageable.pageSize.toLong())
+            .fetchResults()
+
+        return PageImpl(result.results, pageable, result.total)
+    }
 }
