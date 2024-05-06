2897a95281eed66341601a4d8cf4b95dc82154b6
99winnmin <ojysep9987@gmail.com>
2024-05-06 18:33:21 +0900

    :sparkles: feat : controller layer 댓글 정보 dto 추가

---
+++ ko-board/src/main/kotlin/com/korine/koboard/controller/dto/CommentResponse.kt
@@ -0,0 +1,17 @@
+package com.korine.koboard.controller.dto
+
+import com.korine.koboard.service.dto.CommentResponseDto
+
+data class CommentResponse(
+    val id: Long,
+    val content: String,
+    val createdBy: String,
+    val createdAt: String,
+)
+
+fun CommentResponseDto.toResponse() = CommentResponse(
+    id = id,
+    content = content,
+    createdBy = createdBy,
+    createdAt = createdAt,
+)
