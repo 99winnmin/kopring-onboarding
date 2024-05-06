aa77df7d9b356b03c5f6b845feb6b143c0c0e632
99winnmin <ojysep9987@gmail.com>
2024-05-06 18:33:29 +0900

    :sparkles: feat : service layer 댓글 정보 dto 추가

---
+++ ko-board/src/main/kotlin/com/korine/koboard/service/dto/CommentResponseDto.kt
@@ -0,0 +1,17 @@
+package com.korine.koboard.service.dto
+
+import com.korine.koboard.domain.Comment
+
+data class CommentResponseDto(
+    val id: Long,
+    val content: String,
+    val createdBy: String,
+    val createdAt: String,
+)
+
+fun Comment.toResponseDto() = CommentResponseDto(
+    id = id,
+    content = content,
+    createdBy = createdBy,
+    createdAt = createdAt.toString(),
+)
