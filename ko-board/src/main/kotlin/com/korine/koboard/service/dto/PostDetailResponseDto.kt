e98a0a7c4c621b77b1737d8a9b8a4e637764662e
99winnmin <ojysep9987@gmail.com>
2024-05-06 18:32:56 +0900

    :sparkles: feat : service layer 게시글 조회 dto 에 댓글 리스트 추가

--- ko-board/src/main/kotlin/com/korine/koboard/service/dto/PostDetailResponseDto.kt
+++ ko-board/src/main/kotlin/com/korine/koboard/service/dto/PostDetailResponseDto.kt
@@ -8,6 +8,7 @@ data class PostDetailResponseDto(
     val content: String,
     val createdBy: String,
     val createdAt: String,
+    val comments: List<CommentResponseDto>,
 )
 
 fun Post.toDetailResponseDto() = PostDetailResponseDto(
@@ -16,4 +17,5 @@ fun Post.toDetailResponseDto() = PostDetailResponseDto(
     content = content,
     createdBy = createdBy,
     createdAt = createdAt.toString(),
+    comments = comments.map { it.toResponseDto() },
 )
