377058c2d5440348eb9cdf765ff908b4de72330b
99winnmin <ojysep9987@gmail.com>
2024-05-06 14:48:33 +0900

    :sparkles: feat : update, delete service 로직 추가

--- ko-board/src/main/kotlin/com/korine/koboard/service/PostService.kt
+++ ko-board/src/main/kotlin/com/korine/koboard/service/PostService.kt
@@ -1,8 +1,12 @@
 package com.korine.koboard.service
 
+import com.korine.koboard.exception.PostNotDeletableException
+import com.korine.koboard.exception.PostNotFoundException
 import com.korine.koboard.repository.PostRepository
 import com.korine.koboard.service.dto.PostCreateRequestDto
+import com.korine.koboard.service.dto.PostUpdateRequestDto
 import com.korine.koboard.service.dto.toEntity
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -11,8 +15,23 @@ import org.springframework.transaction.annotation.Transactional
 class PostService(
     private val postRepository: PostRepository
 ) {
-    @Transactional(readOnly = false) // 클래스 단위, 함수 단위 둘 다 있을때는 함수 단위로 트랜잭션 적용이 따라감
+    @Transactional // 클래스 단위, 함수 단위 둘 다 있을때는 함수 단위로 트랜잭션 적용이 따라감
     fun createPost(postCreateRequestDto: PostCreateRequestDto): Long {
         return postRepository.save(postCreateRequestDto.toEntity()).id
+    }
+
+    @Transactional
+    fun updatePost(id: Long, postUpdateRequestDto: PostUpdateRequestDto): Long {
+        val post = postRepository.findByIdOrNull(id) ?: throw PostNotFoundException()
+        post.update(postUpdateRequestDto)
+        return id
+    }
+
+    @Transactional
+    fun deletePost(id: Long, deletedBy: String) : Long {
+        val post = postRepository.findByIdOrNull(id) ?: throw PostNotFoundException()
+        if(post.createdBy != deletedBy) throw PostNotDeletableException()
+        postRepository.deleteById(id)
+        return id
     }
 }
