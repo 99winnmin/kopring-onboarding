806d359075367c8cc342413f2bd743bb50a104b0
99winnmin <ojysep9987@gmail.com>
2024-05-06 14:48:04 +0900

    :art: feat : controller layer dto 내에 service layer dto 변환용 확장함수 추가

--- ko-board/src/main/kotlin/com/korine/koboard/dto/PostCreateRequest.kt
+++ ko-board/src/main/kotlin/com/korine/koboard/dto/PostCreateRequest.kt
@@ -1,7 +1,15 @@
 package com.korine.koboard.dto
 
+import com.korine.koboard.service.dto.PostCreateRequestDto
+
 data class PostCreateRequest(
     val title: String,
     val content: String,
     val createdBy: String,
+)
+
+fun PostCreateRequest.toDto() = PostCreateRequestDto(
+    title = title,
+    content = content,
+    createdBy = createdBy,
 )
--- ko-board/src/main/kotlin/com/korine/koboard/dto/PostUpdateRequest.kt
+++ ko-board/src/main/kotlin/com/korine/koboard/dto/PostUpdateRequest.kt
@@ -1,7 +1,15 @@
 package com.korine.koboard.dto
 
+import com.korine.koboard.service.dto.PostUpdateRequestDto
+
 data class PostUpdateRequest(
     val title: String,
     val content: String,
     val updatedBy: String,
+)
+
+fun PostUpdateRequest.toDto() = PostUpdateRequestDto(
+    title = title,
+    content = content,
+    updatedBy = updatedBy,
 )
