050d5937383833d189caae20e57ed12b0cd4e973
99winnmin <ojysep9987@gmail.com>
2024-05-06 14:47:11 +0900

    :zap: fix : 게시판 엔티티 update 함수 구현

--- ko-board/src/main/kotlin/com/korine/koboard/domain/Post.kt
+++ ko-board/src/main/kotlin/com/korine/koboard/domain/Post.kt
@@ -1,5 +1,7 @@
 package com.korine.koboard.domain
 
+import com.korine.koboard.exception.PostNotUpdatableException
+import com.korine.koboard.service.dto.PostUpdateRequestDto
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
@@ -19,4 +21,13 @@ class Post(
         protected set
     var content: String = content
         protected set
+
+    fun update(postupdateRequestDto: PostUpdateRequestDto) {
+        if (postupdateRequestDto.updatedBy != createdBy) {
+            throw PostNotUpdatableException()
+        }
+        title = postupdateRequestDto.title
+        content = postupdateRequestDto.content
+        super.updatedBy(postupdateRequestDto.updatedBy)
+    }
 }
